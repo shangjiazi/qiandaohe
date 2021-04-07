8dc305c74d82e3d9250a3d315b27e226bc5850ff
shangjiazi <64829367+shangjiazi@users.noreply.github.com>
2021-04-07 17:22:36 +0800

    Update smzdm.js

--- scripts/smzdm.js
+++ scripts/smzdm.js
@@ -1,16 +1,29 @@
 //网页端每日签到
 const axios = require("axios");
+const cookie = process.env.SMZDM_COOKIE;
+const fs = require("fs");
+const qmsgapi = process.env.QMSGAPI;
+const sckey = process.env.sckey;
+const tgapi = process.env.tgapi;
+once = null;
+ckstatus = 1;
+result_md = ""
+signstatus = 0;
+time = new Date();
+tmpHours = time.getHours();
+time.setHours(tmpHours + 8);
+notice = time.toLocaleString() + "\n";
+const header = {
+    headers: {
+        Referer: "https://www.smzdm.com/",
+        cookie: `'${cookie}'`,
+        },
+      };
 function smzdm() {
   return new Promise(async (resolve) => {
     try {
       let url =
         "https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?callback=&_=";
-      const header = {
-        headers: {
-          Referer: "https://www.smzdm.com/",
-          cookie: process.env.SMZDM_COOKIE,
-        },
-      };
       let res = await axios.get(url, header);
       if (res.data.error_code == 0) {
         data = `签到成功!\n签到天数: ${res.data.data.checkin_num} | Lv:${res.data.data.rank} | 经验值:${res.data.data.exp}`;
@@ -26,4 +39,5 @@ function smzdm() {
   });
 }
 //smzdm()
-module.exports = smzdm;
+//module.exports = smzdm;
+sign();
