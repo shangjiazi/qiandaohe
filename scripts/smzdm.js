6885caea4884b0a066708e975e28264fae66f18f
shangjiazi <64829367+shangjiazi@users.noreply.github.com>
2021-04-08 14:35:30 +0800

    Update smzdm.js

--- scripts/smzdm.js
+++ scripts/smzdm.js
@@ -1,29 +1,16 @@
 //网页端每日签到
 const axios = require("axios");
-const cookie = process.env.SMZDM_COOKIE;
-const fs = require("fs");
-const qmsgapi = process.env.QMSGAPI;
-const sckey = process.env.sckey;
-const tgapi = process.env.tgapi;
-once = null;
-ckstatus = 1;
-result_md = ""
-signstatus = 0;
-time = new Date();
-tmpHours = time.getHours();
-time.setHours(tmpHours + 8);
-notice = time.toLocaleString() + "\n";
-const header = {
-    headers: {
-        Referer: "https://www.smzdm.com/",
-        cookie: `'${cookie}'`,
-        },
-      };
 function smzdm() {
   return new Promise(async (resolve) => {
     try {
       let url =
         "https://zhiyou.smzdm.com/user/checkin/jsonp_checkin?callback=&_=";
+      const header = {
+        headers: {
+          Referer: "https://www.smzdm.com/",
+          cookie: config.smzdm.cookie,
+        },
+      };
       let res = await axios.get(url, header);
       if (res.data.error_code == 0) {
         data = `签到成功!\n签到天数: ${res.data.data.checkin_num} | Lv:${res.data.data.rank} | 经验值:${res.data.data.exp}`;
@@ -38,24 +25,5 @@ function smzdm() {
     resolve("什么值得买每日签到：\n" + data);
   });
 }
-
-function server(msg) {
-    return new Promise(async (resolve) => {
-        try {
-            let url = `https://sc.ftqq.com/${sckey}.send`
-            let res = await axios.post(url, `text=v2ex签到(づ ●─● )づ${msg}&desp=${msg}`)
-            if (res.data.errmsg == 'success') {
-                console.log('server酱:发送成功')
-            } else {
-                console.log('server酱:发送失败')
-                console.log(res.data)
-            }
-        } catch (err) {
-            console.log(err);
-        }
-        resolve();
-    });
-}
-
 //smzdm()
-//module.exports = smzdm;
+module.exports = smzdm;
